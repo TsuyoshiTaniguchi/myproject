65566279a549e8a530483cb8a1367276819d2a0b
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-05-14 02:08:53 +0000

    1stデプロイ+テスト完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
+  # 必要に応じて削除
+  config.active_job.queue_adapter = :inline 
 end
