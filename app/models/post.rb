eac6c704d2e3e18b71a6604112e56c432eb3bcf3
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-05-18 01:02:49 +0000

    1stデプロイ+テスト完了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  validates :title, presence: true, length: { maximum: 50 }
+
+  validates :title, presence: true
   validates :content, presence: true
 end
+
+
+<%= link_to "削除", post_path(@post), method: :delete, data: { confirm: "本当に削除しますか？" } %>
