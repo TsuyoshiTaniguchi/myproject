9d298077d8d0ca901b1b94f0d9e2de03ccc0d61e
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-05-18 00:50:01 +0000

    1stデプロイ+テスト完了

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :users
-  
-  get "users/mypage", to: "users#mypage"
+  # 認証関連
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # ユーザー関連
+  resources :users, only: [:show]
+
+  # 投稿関連
+  resources :posts do
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+  end
+
+  # 管理者専用ページ
+  namespace :admin do
+    resources :users, only: [:index, :destroy]
+    resources :posts, only: [:index, :destroy]
+    root "dashboard#index"
+  end
+
+  # トップページ
   root 'homes#top'
-  resources :posts
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
