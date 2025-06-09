c43d3f87fd8376a4113c1abc345070602a647599
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-06-09 05:06:28 +0000

    1stデプロイ+テスト完了

--- app/models/daily_report_skill_tag.rb
+++
@@ -1,4 +0,0 @@
-class DailyReportSkillTag < ApplicationRecord
-  belongs_to :daily_report
-  belongs_to :skill_tag
-end
--- app/models/tag.rb
+++
@@ -1,8 +0,0 @@
-class Tag < ApplicationRecord
-  has_many :daily_report_tags
-  has_many :daily_reports, through: :daily_report_tags
-  belongs_to :daily_report
-  belongs_to :skill_tag
-
-end
-
