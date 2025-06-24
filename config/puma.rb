8f44a3d7919b4e7db24a668be83eb748d75edaca
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-06-24 06:30:06 +0000

    Update puma configuration for production: use Unix socket only

--- config/puma.rb
+++ config/puma.rb
@@ -18,7 +18,12 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+if Rails.env.production?
+  # 本番環境では TCP ポートバインドは使わず、Unix ソケットを使用する
+else
+  port ENV.fetch("PORT") { 3000 }
+end
+
 
 # Specifies the `environment` that Puma will run in.
 #
